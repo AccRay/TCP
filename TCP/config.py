46da386cd28ba42b509d7e1f5ed887f0dbaf6452
wyz12234 <wyz12234@github.com>
2023-09-16 12:59:35 +0800

    retry

--- TCP/config.py
+++ TCP/config.py
@@ -8,19 +8,19 @@ class GlobalConfig:
 
 	# data root
 	# root_dir_all = "tcp_carla_data"
-	# root_dir_all = "/media/ubuntu2204/A582B933E386E737/data"
-	root_dir_all = "/home/ubuntu2204/TCP-main/TCP/data"
+	root_dir_all = "/home/wyz/TCP/data"
 
 	# train_towns = ['town01', 'town03', 'town04',  'town06', ]
 	# val_towns = ['town02', 'town05', 'town07', 'town10']
 	train_towns = ['town01']
-	val_towns = ['town02']
+	val_towns = ['town01']
 	# val_towns = ['town01_addition']
 	
 	train_data, val_data = [], [] 
 	for town in train_towns:		
 		train_data.append(os.path.join(root_dir_all, town))
-		train_data.append(os.path.join(root_dir_all, town+'_addition'))
+		if town not in ['town02', 'town05']:
+			train_data.append(os.path.join(root_dir_all, town+'_addition'))
 	for town in val_towns:
 		val_data.append(os.path.join(root_dir_all, town+'_val'))
 
